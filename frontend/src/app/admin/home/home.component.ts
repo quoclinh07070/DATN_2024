40007432ce5d4f31d2b5cafbb1b91991baa9321f
Beo Học Ngu <100513440+BeoDepTraii@users.noreply.github.com>
2024-11-11 18:07:43 +0700

    add checkRole

--- frontend/src/app/admin/home/home.component.ts
+++ frontend/src/app/admin/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,20 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./home.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // Add this line
 })
-export class HomeComponent {
-  // Your component logic
+export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}  // Inject AuthService and Router
+
+  ngOnInit(): void {
+    this.authService.checkUserRole().subscribe(
+      (isAuthorized: boolean) => {
+        if (!isAuthorized) {
+          this.router.navigate(['/admin/login']);
+        }
+      },
+      (error) => {
+        console.error('Lỗi kiểm tra quyền:', error);
+        this.router.navigate(['/login']);
+      }
+    );
+  }
 }
